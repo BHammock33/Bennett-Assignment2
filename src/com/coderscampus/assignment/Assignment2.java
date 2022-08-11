efb1952e976dd775467a5f3b0778e4483482aea1
BHammock33 <Benhamm22alt@gmail.com>
2022-08-11 12:52:55 -0400

    first A2 commit

---
+++ src/com/coderscampus/assignment/Assignment2.java
@@ -0,0 +1,10 @@
+package com.coderscampus.assignment;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+changes being tracked?
+	}
+
+}
