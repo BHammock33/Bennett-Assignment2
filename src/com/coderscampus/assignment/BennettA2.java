bd8d960fcad0b4914ef333a79888af515951aa65
BHammock33 <Benhamm22alt@gmail.com>
2022-08-15 09:41:58 -0400

    Assignment 2
    finalized assignment 2

---
+++ src/com/coderscampus/assignment/BennettA2.java
@@ -0,0 +1,36 @@
+package com.coderscampus.assignment;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class BennettA2 {
+
+	public static void main(String[] args) {
+		int answer, guess;
+		final int MAX = 100;
+		Scanner in = new Scanner(System.in);
+		Random target = new Random();
+		boolean win = false;
+		answer = target.nextInt(MAX) + 1;
+		int chances = 5;
+		System.out.println(answer);
+
+		while (!win && chances > 0) {
+			System.out.println("Guess a number between 1 and 100: ");
+			guess = in.nextInt();
+			chances--;
+			if (guess > answer && chances != 0) {
+				System.out.println("Too high, try again");
+			} else if (guess < answer && chances != 0) {
+				System.out.println("Too low, try again");
+			} else if (guess == answer) {
+				System.out.println("You guessed the number");
+				win = true;
+			} else if (chances == 0 && win != true) {
+				System.out.println("Bummer you lose");
+				break;
+			}
+		}
+	}
+
+}
